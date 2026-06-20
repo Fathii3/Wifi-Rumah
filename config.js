238265f414908844c7c99736f03f95ee8d45e022
Fathii3 <fathifadhil996@gmail.com>
2026-06-20 18:33:47 +0700

    ganti sandi

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
-// config.js
 // Exporting configuration for the Wi-Fi QR Code
 const wifiConfig = {
     ssid: "Wifi-Rumah",
     password: "Rumah321",
-    encryption: "WPA" // Options: WPA, WEP, or nopass (for open networks)
+    encryption: "WPA"
 };
